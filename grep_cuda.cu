e40aaaabb6e64bdfa055fbb04917447be68fc765
kavya <soldatargent@gmail.com>
2018-11-01 21:56:30 +0530

    fixed bug in last match not being printed

--- grep_cuda.cu
+++ grep_cuda.cu
@@ -6,7 +6,7 @@ extern "C" {
 }
 
 #define MAX_FILE_SIZE 1 << 30
-const int CHUNK = 5000;
+const int CHUNK = 23;
 
 typedef struct search_result {
   char* context;
@@ -19,6 +19,7 @@ __global__ void grep_kernel(char** contents, res*** results, const char* pattern
   int line = 1;
 
   /* Local variables that keep track of the start and end of the context */
+  /* TODO: out_before: needs to be initialized by going back until previous newline is found */
   int out_before = -1;
   uint8_t matched = 0;
 
@@ -26,7 +27,12 @@ __global__ void grep_kernel(char** contents, res*** results, const char* pattern
   char* start = &(contents[file_no][threadIdx.x * CHUNK]);
   res* result_loc = &(results[file_no][threadIdx.x][0]);
   char c;
-  for(int i = 0; (c = *(start + i)) != '\0'; i++){
+
+  int i;
+  for(i = 0; i < threadIdx.x * CHUNK && *(start-i) != '\n'; i++);
+  out_before = -1 * i;
+
+  for(i = 0; i < CHUNK && ((c = *(start + i)) != '\0'); i++){
 	valid = 1;
 	line += (c == '\n');
 	if(matched && (c == '\n')){
@@ -52,6 +58,13 @@ __global__ void grep_kernel(char** contents, res*** results, const char* pattern
 	/* Need to remember whether some valid match occured on this line before - so || to not lose previous data */
 	matched = matched || (valid != 0);
   }
+
+  /* There might be some matched string still waiting to find its ending newline character */
+  if(matched){
+	for(; (c = *(start + i) != '\n'); i++);
+	  memcpy((result_loc + res_idx)->context, (void*)(start + out_before+1), i - out_before - 1);
+	  (result_loc + res_idx)->line = line - 1;
+  }
 }
 
 extern "C" void parallel_grep(char** file_names, file_info* info, int n_files, char* pattern){
@@ -60,12 +73,12 @@ extern "C" void parallel_grep(char** file_names, file_info* info, int n_files, c
   char** temp = (char**) malloc(n_files * sizeof(char*));
   cudaMalloc(&device_contents, n_files * sizeof(char*));
 
-  /* cudaStream_t streams[n_files]; */
+  cudaStream_t streams[n_files];
   for(int i = 0; i < n_files; i++){
 	cudaMalloc(&temp[i], MAX_FILE_SIZE * sizeof(char));
 	cudaMemcpy(temp[i], info[i].mmap, info[i].size, cudaMemcpyHostToDevice);
 	cudaMemcpy(device_contents + i, &(temp[i]), sizeof(char*), cudaMemcpyHostToDevice);
-	/* cudaStreamCreate(&streams[i]); */
+	cudaStreamCreate(&streams[i]);
 	/* cudaMallocHost(&(info[i].mmap), (size_t) info[i].size); */
 	/* cudaMallocHost(&temp[i], info[i].size); */
 	/* cudaMemcpyAsync(temp[i], info[i].mmap, info[i].size, cudaMemcpyHostToDevice, streams[i]); */
@@ -96,25 +109,23 @@ extern "C" void parallel_grep(char** file_names, file_info* info, int n_files, c
 	  }
 	}
   }
+  printf("%d\n", threads_size[0]);
 
-  /* for(int i = 0; i < n_files; i++){ */
-  /* grep_kernel <<< 1, threads_size[i], 0, streams[i] >>> (device_contents, results, results_size, device_pattern, i); */
-  /* Unpinning the memory */
-  /* cudaFreeHost(info[i].mmap); */
-  /* } */
-
-  grep_kernel <<< 1, 1 >>> (device_contents, results, device_pattern, 0);
+  for(int i = 0; i < n_files; i++){
+	grep_kernel <<< 1, threads_size[i], 0, streams[i] >>> (device_contents, results, device_pattern, i);
+	/* Unpinning the memory */
+	/* cudaFreeHost(info[i].mmap); */
+  }
 
   cudaDeviceSynchronize();
 
   res result;
   for(int i = 0; i < n_files; i++){
 	for(int j = 0; j < threads_size[i]; j++){
-	  for(int k = 0; ; k++){
+	  for(int k = 0; k < 20; k++){
 		result = results[i][j][k];
-		if(result.line == 0)
-		  break;
-		printf("%s:%d:%s\n", file_names[i], result.line, result.context);
+		if(result.line != 0)
+		  printf("%s:%s\n", file_names[i], result.context);
 	  }
 	}
   }
